00d10f65782aade52e163202545a3c2fcc3475d3
Mehman Aliyev <mehman_va@mail.ru>
2023-10-13 11:02:09 +0400

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/components/button.scss"
+
+const Button = () => {
+  return (
+    <div>
+      <button className='btn'>Click</button>
+    </div>
+  )
+}
+
+export default Button
